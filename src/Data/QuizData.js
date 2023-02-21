0d67af2956f1bd4ebd072058e868604e57928b8c
RaheemRoshanR <120947563+RaheemRoshanR@users.noreply.github.com>
2023-02-21 16:16:19 +0500

    Add files via upload

---
+++ src/Data/QuizData.js
@@ -0,0 +1,42 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "In CSS, h1 can be called as?",
+        options: ["Selector", "Value", "Attribute", "Tag"],
+        answer:1
+    },
+    {
+        question: "Which HTML tag is used to define an internal style sheet?",
+        options: ["css", "text/style", "style", "script"],
+        answer:3
+    },
+    {
+        question: "CSS padding property is used for?",
+        options: ["Border", "Space", "Margin", "Background color"],
+        answer:2
+    },
+    {
+        question: "In CSS, “font-size” can be called as?",
+        options: ["Selector", "Rule", "Property", "Property-Name"],
+        answer:4
+    },
+];
